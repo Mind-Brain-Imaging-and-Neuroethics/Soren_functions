dc1898b8637555c0fec76ef6cddbe0fee0e1735c
= <sorenwt@gmail.com>
2019-05-21 17:08:34 -0400

    ANOVA options for ft_measurestatistics

--- ft_measurestatistics.m
+++ ft_measurestatistics.m
@@ -7,10 +7,10 @@ function [stats] = ft_measurestatistics(cfg,data)
 % cfg: a config structure with the following options:
 %      test: the test you want to use, passed in as a string. Inputs
 %         include 'ttest' (for paired samples), 'ttest2' (for unpaired),
-%         'signrank', 'ranksum', 'anova', 'rmanova', 'kruskalwallis', 
-%         'friedman', and 'empirical'. Use empirical for comparing one 
-%         outputs structure with a fourth dimension (either surrogates or  
-%         subsamples) with another outputs structure with only three 
+%         'signrank', 'ranksum', 'anova', 'rmanova', 'kruskalwallis',
+%         'friedman', and 'empirical'. Use empirical for comparing one
+%         outputs structure with a fourth dimension (either surrogates or
+%         subsamples) with another outputs structure with only three
 %         dimensions. (default = 'ranksum' or 'kruskal-wallis' depending on
 %         the length of data)
 %      channel: the channels you want to test - must be an input that works
@@ -44,9 +44,9 @@ function [stats] = ft_measurestatistics(cfg,data)
 
 if ~cfgcheck(cfg,'test')
     if length(data) == 2
-    cfg.test = 'ranksum';
+        cfg.test = 'ranksum';
     else
-       cfg.test = 'kruskalwallis'; 
+        cfg.test = 'kruskalwallis';
     end
 end
 
@@ -67,8 +67,8 @@ if ~cfgcheck(cfg,'effectsize')
             cfg.effectsize = 'auroc';
         case 'signrank'
             cfg.effectsize = 'rbcorr';
-        %case 'anova','kruskalwallis','friedman'
-        %    cfg.effectsize = 'psi';
+        case 'anova','kruskalwallis','friedman'
+            cfg.effectsize = 'psi';
     end
 end
 
@@ -83,9 +83,9 @@ if cfgcheck(cfg,'multcompare','cluster') && ~cfgcheck(cfg.cluster,'statfun')
         case 'signrank'
             cfg.cluster.statfun = 'ft_statfun_signrank';
         case 'anova'
-            cfg.cluster.statfun = 'ft_statfun_indepsamplesF';
+            cfg.cluster.statfun = 'ft_statfun_indepsamplesF'; % probably broken
         case 'rmanova'
-            cfg.cluster.statfun = 'ft_statfun_depsamplesF';
+            cfg.cluster.statfun = 'ft_statfun_depsamplesF'; %broken
             warning('Repeated-measures anova not currently supported')
         case 'kruskalwallis'
             cfg.cluster.statfun = 'ft_statfun_kruskal';
@@ -135,17 +135,36 @@ for i = 1:length(data{1}.meas)
                 case 'signrank'
                     stats{i}.p(c) = signrank(data{1}.data(:,c,i),data{2}.data(:,c,i));
                 case 'anova'
-                    
+                    dat = []; design = [];
+                    for cc = 1:length(data)
+                        dat = cat(2,dat,horz(data{cc}.data(:,c,i)));
+                        design = cat(2,design,ones(1,length(data{cc}.data(:,c,i)))*cc);
+                    end
+                    stats{i}.p(c) = anovan(dat,design,'off');
                 case 'rmanova'
-                    
+                    % not implemented yet
                 case 'kruskalwallis'
-                    
+                    dat = []; design = [];
+                    for cc = 1:length(data)
+                        dat = cat(2,dat,horz(data{cc}.data(:,c,i)));
+                        design = cat(2,design,ones(1,length(data{cc}.data(:,c,i)))*cc);
+                    end
+                    stats{i}.p(c) = kruskalwallis(dat,design,'off');
                 case 'friedman'
-                    
+                    dat = [];
+                    for cc = 1:length(data)
+                        dat(cc,:) = vert(data{cc}.data(:,c,i));
+                    end
+                    stats{i}.p(c) = friedman(dat,1,'off');
                 case 'empirical'
                     % finish later
             end
-            stats{i}.effsize{c} = mes(data{1}.data(:,c,i),data{2}.data(:,c,i),cfg.effectsize);
+            switch cfg.test
+                case 'ttest','ttest2','ranksum','signrank'
+                    stats{i}.effsize{c} = mes(data{1}.data(:,c,i),data{2}.data(:,c,i),cfg.effectsize);
+                case 'anova','rmanova','kruskalwallis','friedman'
+                    % not implemented yet
+            end
         end
         
         switch cfg.multcompare
@@ -166,17 +185,47 @@ for i = 1:length(data{1}.meas)
     elseif cfgcheck(cfg,'multcompare','mean')
         switch cfg.test
             case 'ttest'
-                [~,stats{i}.p] =  ttest(mean(data{1}.data(:,:,i),2)-mean(data{2}.data(:,:,i),2));
+                [~,stats{i}.p] =  ttest(nanmean(data{1}.data(:,:,i),2)-nanmean(data{2}.data(:,:,i),2));
             case 'ttest2'
-                [~,stats{i}.p] = ttest2(mean(data{1}.data(:,:,i),2),mean(data{2}.data(:,:,i),2));
+                [~,stats{i}.p] = ttest2(nanmean(data{1}.data(:,:,i),2),nanmean(data{2}.data(:,:,i),2));
             case 'ranksum'
-                stats{i}.p = ranksum(mean(data{1}.data(:,:,i),2),mean(data{2}.data(:,:,i),2));
+                stats{i}.p = ranksum(nanmean(data{1}.data(:,:,i),2),nanmean(data{2}.data(:,:,i),2));
             case 'signrank'
-                stats{i}.p = signrank(mean(data{1}.data(:,:,i),2),mean(data{2}.data(:,:,i),2));
+                stats{i}.p = signrank(nanmean(data{1}.data(:,:,i),2),nanmean(data{2}.data(:,:,i),2));
+            case 'anova'
+                dat = []; design = [];
+                for cc = 1:length(data)
+                    dat = cat(2,dat,horz(nanmean(data{cc}.data(:,:,i),2)));
+                    design = cat(2,design,ones(1,length(data{cc}.data(:,1,i)))*cc);
+                end
+                stats{i}.p = anovan(dat,design,'off');
+            case 'rmanova'
+                % not implemented yet
+            case 'kruskalwallis'
+                dat = []; design = [];
+                for cc = 1:length(data)
+                    dat = cat(2,dat,horz(nanmean(data{cc}.data(:,:,i),2)));
+                    design = cat(2,design,ones(1,length(data{cc}.data(:,1,i)))*cc);
+                end
+                stats{i}.p = kruskalwallis(dat,design,'off');
+            case 'friedman'
+                dat = [];
+                for cc = 1:length(data)
+                    dat(cc,:) = vert(nanmean(data{cc}.data(:,:,i),2));
+                end
+                stats{i}.p = friedman(dat,1,'off');
+            case 'empirical'
+                % finish later
+                
             case 'empirical'
                 % finish later
         end
-        stats{i}.effsize = mes(mean(data{1}.data(:,:,i),2),mean(data{2}.data(:,:,i),2),cfg.effectsize);
+        switch cfg.test
+            case 'ttest','ttest2','ranksum','signrank'
+                stats{i}.effsize = mes(mean(data{1}.data(:,:,i),2),mean(data{2}.data(:,:,i),2),cfg.effectsize);
+            case 'anova','rmanova','kruskalwallis','friedman'
+                % not implemented yet
+        end
     end
 end
 
