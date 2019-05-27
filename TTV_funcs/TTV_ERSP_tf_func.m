01d0812ef7d1093493adbd8929348097a2900343
SorenWT <sorenwt@gmail.com>
2019-05-27 17:03:21 -0400

    Added lpinstabilityfix

--- TTV_funcs/TTV_ERSP_tf_func.m
+++ TTV_funcs/TTV_ERSP_tf_func.m
@@ -36,7 +36,7 @@ parfor i = 1:length(files)
                 if ~isempty(freqs{q})
                     cfg = [];
                     if isnan(freqs{q}(1))
-                        cfg.lpfilter = 'yes'; cfg.lpfreq = freqs{q}(2);
+                        cfg.lpfilter = 'yes'; cfg.lpfreq = freqs{q}(2); cfg.lpinstabilityfix = 'split';
                     else
                         cfg.bpfilter = 'yes'; cfg.bpfreq = freqs{q}; cfg.bpinstabilityfix = 'split';
                     end
