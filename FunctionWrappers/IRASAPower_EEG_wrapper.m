9c89a3fc0ff752822ed251276bdd1069ff94d396
= <sorenwt@gmail.com>
2019-07-07 21:55:23 -0400

    Cruncher edits 05/07/2019

--- FunctionWrappers/IRASAPower_EEG_wrapper.m
+++ FunctionWrappers/IRASAPower_EEG_wrapper.m
@@ -1,6 +1,10 @@
-function [OsciFrac] = IRASAPower_EEG_wrapper(spec,oscifrac,frange,normalize)
+function [OsciFrac] = IRASAPower_EEG_wrapper(spec,oscifrac,frange,normalize,method)
 
 OsciFrac = zeros(1,size(spec.osci,2));
+
+if nargin < 5
+    method = 'mean';
+end
 
 if nargin < 4
     normalize = 'no';
@@ -14,7 +18,7 @@ disp(' ')
 if strcmpi(oscifrac,'osci')
     disp('Computing oscillatory power...')
 else
-   disp('Computing fractal power...') 
+    disp('Computing fractal power...')
 end
 
 freqindex = intersect(find(spec.freq > frange(1)),find(spec.freq < frange(2)));
@@ -27,11 +31,23 @@ for c = 1:size(spec.frac,2)
     %frac = sgolayfilt(spec.frac(:,c),5,1501);
     pow = spec.(oscifrac)(freqindex,c);
     
-    OsciFrac(c) = trapz(freqs,pow);
+    if strcmpi(method,'trapz')
+        OsciFrac(c) = trapz(freqs,pow);
+    elseif strcmpi(method,'mean')
+        OsciFrac(c) = nanmean(pow);
+    end
     if strcmpi(normalize,'mixd')
-        OsciFrac(c) = OsciFrac(c)/trapz(freqs,spec.mixd(freqindex,c));
+        if strcmpi(method,'trapz')
+            OsciFrac(c) = OsciFrac(c)/trapz(freqs,spec.mixd(freqindex,c));
+        elseif strcmpi(method,'mean')
+            OsciFrac(c) = OsciFrac(c)/nanmean(spec.mixd(freqindex,c));
+        end
     elseif iscell(normalize)
         newfreqs = intersect(find(spec.freq > normalize{2}(1)),find(spec.freq < normalize{2}(2)));
-        OsciFrac(c) = OsciFrac(c)/trapz(spec.freq(newfreqs),spec.(normalize{1})(newfreqs,c));
+        if strcmpi(method,'trapz')
+            OsciFrac(c) = OsciFrac(c)/trapz(spec.freq(newfreqs),spec.(normalize{1})(newfreqs,c));
+        elseif strcmpi(method,'mean')
+            OsciFrac(c) = OsciFrac(c)/nanmean(spec.(normalize{1})(newfreqs,c));
+        end
     end
 end
